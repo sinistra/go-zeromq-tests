7362be68909c9213ee94aab7934f6faf5525f7ac
Paulo Ferreira <paulo.bsf@gmail.com>
2014-10-18 14:04:26 +0100

    Hello World Server Example from 0mq guide & pebbe/zmq4

---
+++ client/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	zmq "github.com/pebbe/zmq4"
+)
+
+func main() {
+	// Socket to talk to server
+	fmt.Println("Connecting to hello world server...")
+
+	requester, _ := zmq.NewSocket(zmq.REQ)
+	defer requester.Close()
+
+	requester.Connect("tcp://localhost:5555")
+
+	for request_nbr := 0; request_nbr != 10; request_nbr++ {
+		// send hello
+		msg := fmt.Sprintf("Hello %d", request_nbr)
+		fmt.Println("Sending ", msg)
+		requester.Send(msg, 0)
+
+		// Wait for reply:
+		reply, _ := requester.Recv(0)
+		fmt.Println("Received ", reply)
+	}
+}
